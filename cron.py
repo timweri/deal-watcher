d03b4c041cee02ecf06b5303af50c39cc7291314
Duc Thanh Nguyen <ng.duc.tahn@gmail.com>
2022-03-03 15:54:02 -0500

    Adjust heartbeat

--- cron.py
+++ cron.py
@@ -13,7 +13,7 @@ clear_job = cron.new(command="python clean.py")
 clear_job.minute.every(30)
 
 heartbeat_job = cron.new(command="python heartbeat.py")
-heartbeat_job.hour.every(3)
+heartbeat_job.hour.every(1)
 
 cron.write()
 for result in cron.run_scheduler():
