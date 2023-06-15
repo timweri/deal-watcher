8270b1103e781738a22f34dcf54549eb2dde77ff
Duc Thanh Nguyen <ng.duc.tahn@gmail.com>
2023-06-15 01:52:33 -0400

    Add delay

--- notify.py
+++ notify.py
@@ -1,6 +1,7 @@
 import telegram
 from dotenv import load_dotenv
 import os
+import time
 load_dotenv()
 
 ACCESS_TOKEN = os.getenv("TELEGRAM_ACCESS_TOKEN")
@@ -9,3 +10,4 @@ bot = telegram.Bot(token=ACCESS_TOKEN)
 
 def notify(message):
     bot.send_message(CHAT_ID, text=message)
+    time.sleep(1)
