a28d5df7b9a097ace0f07b44ba9a73dc6dce78d3
Duc Thanh Nguyen <ng.duc.tahn@gmail.com>
2023-06-15 01:48:51 -0400

    Fix

--- fetch_reddit.py
+++ fetch_reddit.py
@@ -11,12 +11,13 @@ TIME_WINDOW = int(os.getenv('TIME_WINDOW'))
 bapcsalescanada_url = "https://www.reddit.com/r/bapcsalescanada/new.json"
 canadianhardwareswap_url = "https://www.reddit.com/r/CanadianHardwareSwap/new.json"
 
+FILE_NAME = 'cache.json'
 sites = [bapcsalescanada_url, canadianhardwareswap_url]
 
 try:
-    with open('./cache.json', 'r') as f:
+    with open(FILE_NAME, 'r') as f:
         cache = json.load(f)
-except FileNotFoundError:
+except:
     cache = {}
 
 for site in sites:
@@ -28,7 +29,7 @@ for site in sites:
     for post in res['data']['children']:
         post_data = post['data']
         post_id = post_data['id']
-        post_created = post_data['created']
+        post_created = int(post_data['created'])
         if post_created < time.time() - TIME_WINDOW or post_id in cache:
             continue
         cache[post_id] = post_created
@@ -46,5 +47,5 @@ for site in sites:
         
         notify(message)
 
-with open('./cache.json', 'w') as outfile:
+with open(FILE_NAME, 'w') as outfile:
     json.dump(cache, outfile)
